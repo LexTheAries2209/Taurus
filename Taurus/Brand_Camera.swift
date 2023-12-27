d6361424d1d75724c41b1e2e0688f6d6c1999d85
LexTheAries2209 <673661658@qq.com>
2023-12-27 18:51:43 +0800

    1.2.1的release版

--- Taurus/Brand_Camera.swift
+++ Taurus/Brand_Camera.swift
@@ -19,8 +19,8 @@ import Foundation
         //"Nikon" : ["Z 9","Z 8","Z f","Z 7II","Z 7","Z 6II","Z 6","Z 5","Z fc","Z 50","Z 30"],
         //"Blackmagicdesign" : ["Blackmagic URSA Mini Pro 12K [OLPF]","Blackmagic URSA Mini Pro 12K","Blackmagic URSA Mini Pro 4.6K G2","Blackmagic URSA Mini Pro 4.6K","Blackmagic Cinema Camera 6K","Blackmagic Pocket Cinema Camera 6K Pro","Blackmagic Pocket Cinema Camera 6K G2","Blackmagic Pocket Cinema Camera 6K","Blackmagic Pocket Cinema Camera 4K"],
         //"Fujifilm" : ["GFX100 II","X-H2S","X-H2","X-T5"],
-        //"Panasonic" : ["S1H"],
-        "Panasonic" : ["S5M2X","S5M2","GH6","G9M2"],
+        //"Panasonic" : ["S1H","GH6","G9M2"],
+        "Panasonic" : ["S5M2X","S5M2"],
         "DJI" : ["Ronin 4D[8K]","Ronin 4D[6K]","Inspire 3"],
         //"ZCam" : [""]
     ]
--- Taurus/func_PanaResolution.swift
+++ Taurus/func_PanaResolution.swift
@@ -9,19 +9,7 @@ import Foundation
 func PanaResolution(cameradata:CameraData) -> [String] {
     var PanaResolution = [""]
     if cameradata.CameraName == "S5M2" || cameradata.CameraName == "S5M2X"{
-        if cameradata.Codec == "Prores 422 HQ" {
-            switch cameradata.Format {
-            case "Full Frame" :
-                PanaResolution = ["DCI 5.8K[5776*3056][10bit 4:2:2][23.98p]","DCI 5.8K[5776*3056][10bit 4:2:2][24p]","DCI 5.8K[5776*3056][10bit 4:2:2][25p]","DCI 5.8K[5776*3056][10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][23.98p]","DCI 4K[10bit 4:2:2][24p]","DCI 4K[10bit 4:2:2][25p]","DCI 4K[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][23.98p]","FHD[10bit 4:2:2][24p]","FHD[10bit 4:2:2][25p]","FHD[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][50p]","FHD[10bit 4:2:2][59.94p]"]
-            case "APS-C" :
-                PanaResolution = ["DCI 4K[10bit 4:2:2][23.98p]","DCI 4K[10bit 4:2:2][24p]","DCI 4K[10bit 4:2:2][25p]","DCI 4K[10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][50p]","DCI 4K[10bit 4:2:2][59.94p]","3.3K 4:3[3328*2496][10bit 4:2:2][23.98p]","3.3K 4:3[3328*2496][10bit 4:2:2][24p]","3.3K 4:3[3328*2496][10bit 4:2:2][25p]","3.3K 4:3[3328*2496][10bit 4:2:2][29.97p]","3.3K 4:3[3328*2496][10bit 4:2:2][50p]","FHD[10bit 4:2:2][24p]","FHD[10bit 4:2:2][25p]","FHD[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][50p]","FHD[10bit 4:2:2][59.94p]"]
-            case "Pixel to Pixel" :
-                PanaResolution = ["DCI 4K[10bit 4:2:2][23.98p]","DCI 4K[10bit 4:2:2][24p]","DCI 4K[10bit 4:2:2][25p]","DCI 4K[10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][50p]","DCI 4K[10bit 4:2:2][59.94p]","3.3K 4:3[3328*2496][10bit 4:2:2][23.98p]","3.3K 4:3[3328*2496][10bit 4:2:2][24p]","3.3K 4:3[3328*2496][10bit 4:2:2][25p]","3.3K 4:3[3328*2496][10bit 4:2:2][29.97p]","3.3K 4:3[3328*2496][10bit 4:2:2][50p]","FHD[10bit 4:2:2][24p]","FHD[10bit 4:2:2][25p]","FHD[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][50p]","FHD[10bit 4:2:2][59.94p]"]
-            default :
-                PanaResolution = [""]
-            }
-        }
-        if cameradata.Codec == "Prores 422" {
+        if cameradata.Codec == "Prores 422 HQ" || cameradata.Codec == "Prores 422"{
             switch cameradata.Format {
             case "Full Frame" :
                 PanaResolution = ["DCI 5.8K[5776*3056][10bit 4:2:2][23.98p]","DCI 5.8K[5776*3056][10bit 4:2:2][24p]","DCI 5.8K[5776*3056][10bit 4:2:2][25p]","DCI 5.8K[5776*3056][10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][23.98p]","DCI 4K[10bit 4:2:2][24p]","DCI 4K[10bit 4:2:2][25p]","DCI 4K[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][23.98p]","FHD[10bit 4:2:2][24p]","FHD[10bit 4:2:2][25p]","FHD[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][50p]","FHD[10bit 4:2:2][59.94p]"]
@@ -70,12 +58,12 @@ func PanaResolution(cameradata:CameraData) -> [String] {
             }
         }
     }
-//    if cameradata.CameraName == "GH6" {
-//        if cameradata.Codec == "Prores 422 HQ" {
-//            if cameradata.Format == "M43" {
-//                
-//            }
-//        }
-//    }
+    if cameradata.CameraName == "GH6" {
+        if cameradata.Codec == "Prores 422 HQ" || cameradata.Codec == "Prores 422" {
+            if cameradata.Format == "M43" {
+                PanaResolution = ["DCI 5.7K[5728*3024][10bit 4:2:2][23.98p]","DCI 5.7K[5728*3024][10bit 4:2:2][24p]","DCI 5.7K[5728*3024][10bit 4:2:2][25p]","DCI 5.7K[5728*3024][10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][23.98p]","DCI 4K[10bit 4:2:2][24p]","DCI 4K[10bit 4:2:2][25p]","DCI 4K[10bit 4:2:2][29.97p]","DCI 4K[10bit 4:2:2][50p]","DCI 4K[10bit 4:2:2][59.94p]","FHD[10bit 4:2:2][23.98p]","FHD[10bit 4:2:2][24p]","FHD[10bit 4:2:2][25p]","FHD[10bit 4:2:2][29.97p]","FHD[10bit 4:2:2][50p]","FHD[10bit 4:2:2][59.94p]"]
+            }
+        }
+    }
     return PanaResolution
 }
