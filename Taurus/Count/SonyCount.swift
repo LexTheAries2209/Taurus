d90bc71e95544cfe36538f9f0dee42575b615db2
LexTheAries2209 <673661658@qq.com>
2026-05-26 17:13:56 +0800

    加入了Sony A7R6和A7M5的选择和计算。

--- Taurus/Count/SonyCount.swift
+++ Taurus/Count/SonyCount.swift
@@ -26,11 +26,11 @@ func SonyCodecSpeed(cameradata:CameraData) -> Double {
         return 111
     case "XAVC S 4K_UHD FF[10bit 4:2:2][25p/29.97p]","XAVC S 4K_UHD S35[10bit 4:2:2][25p/29.97p]","XAVC S 4K_UHD FF[10bit 4:2:2][25p/29.97p][From 6K]","XAVC S 4K_UHD FF[10bit 4:2:2][25p/29.97p][From 7K]" :
         return 140
-    case "XAVC HS 4K_UHD FF[10bit 4:2:0][50p/59.94p]","XAVC S 4K_UHD FF[8bit 4:2:0][50p/59.94p]","XAVC HS 4K_UHD S35[10bit 4:2:0][50p/59.94p]","XAVC S 4K_UHD S35[8bit 4:2:0][50p/59.94p]","XAVC HS 4K_UHD FF[10bit 4:2:0][50p/59.94p][From 6K]","XAVC S 4K_UHD FF[8bit 4:2:0][50p/59.94p][From 6K]" :
+    case "XAVC HS 4K_UHD FF[10bit 4:2:0][50p/59.94p]","XAVC S 4K_UHD FF[8bit 4:2:0][50p/59.94p]","XAVC HS 4K_UHD S35[10bit 4:2:0][50p/59.94p]","XAVC S 4K_UHD S35[8bit 4:2:0][50p/59.94p]","XAVC HS 4K_UHD FF[10bit 4:2:0][50p/59.94p][From 6K]","XAVC HS 4K_UHD FF[10bit 4:2:0][50p/59.94p][From 7K]","XAVC S 4K_UHD FF[8bit 4:2:0][50p/59.94p][From 6K]","XAVC S 4K_UHD FF[8bit 4:2:0][50p/59.94p][From 7K]" :
         return 150
     case "XAVC S-I HD_FHD FF[10bit 4:2:2][50p]","XAVC S-I HD_FHD S35[10bit 4:2:2][50p]" :
         return 185
-    case "XAVC HS 4K_UHD FF[10bit 4:2:2][50p/59.94p]","XAVC HS 4K_UHD FF[10bit 4:2:0][100p/119.88p]","XAVC S 4K_UHD FF[10bit 4:2:2][50p/59.94p]","XAVC S 4K_UHD FF[8bit 4:2:0][100p/119.88p]","XAVC HS 4K_UHD S35[10bit 4:2:2][50p/59.94p]","XAVC HS 4K_UHD S35[10bit 4:2:0][100p/119.88p]","XAVC S 4K_UHD S35[10bit 4:2:2][50p/59.94p]","XAVC S 4K_UHD S35[8bit 4:2:0][100p/119.88p]","XAVC HS 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 6K]","XAVC S 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 6K]" :
+    case "XAVC HS 4K_UHD FF[10bit 4:2:2][50p/59.94p]","XAVC HS 4K_UHD FF[10bit 4:2:0][100p/119.88p]","XAVC S 4K_UHD FF[10bit 4:2:2][50p/59.94p]","XAVC S 4K_UHD FF[8bit 4:2:0][100p/119.88p]","XAVC HS 4K_UHD S35[10bit 4:2:2][50p/59.94p]","XAVC HS 4K_UHD S35[10bit 4:2:0][100p/119.88p]","XAVC S 4K_UHD S35[10bit 4:2:2][50p/59.94p]","XAVC S 4K_UHD S35[8bit 4:2:0][100p/119.88p]","XAVC HS 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 6K]","XAVC HS 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 7K]","XAVC S 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 6K]","XAVC S 4K_UHD FF[10bit 4:2:2][50p/59.94p][From 7K]" :
         return 200
     case "XAVC S-I HD_FHD FF[10bit 4:2:2][59.94p]","XAVC S-I HD_FHD S35[10bit 4:2:2][59.94p]" :
         return 222
@@ -42,13 +42,13 @@ func SonyCodecSpeed(cameradata:CameraData) -> Double {
         return 280
     case "XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][29.97p]","XAVC S-I 4K_UHD FF[10bit 4:2:2][29.97p]","XAVC S-I DCI 4K_DCI 4K S35[10bit 4:2:2][29.97p]","XAVC S-I 4K_UHD S35[10bit 4:2:2][29.97p]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][29.97p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][29.97p][From 6K]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][29.97p][From 7K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][29.97p][From 7K]" :
         return 300
-    case "XAVC HS 8K_8K FF[10bit 4:2:0][23.976p/25p/29.97p][From 8.6K]","XAVC HS 8K_8K FF[10bit 4:2:0][23.976p/25p]" :
+    case "XAVC HS 8K_8K FF[10bit 4:2:0][23.976p/25p/29.97p][From 8.6K]","XAVC HS 8K_8K FF[10bit 4:2:0][23.976p/25p/29.97p][From 8.2K]","XAVC HS 8K_8K FF[10bit 4:2:0][23.976p/25p]" :
         return 400
-    case "XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][50p]","XAVC S-I 4K_UHD FF[10bit 4:2:2][50p]","XAVC S-I DCI 4K_DCI 4K S35[10bit 4:2:2][50p]","XAVC S-I 4K_UHD S35[10bit 4:2:2][50p]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][50p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][50p][From 6K]" :
+    case "XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][50p]","XAVC S-I 4K_UHD FF[10bit 4:2:2][50p]","XAVC S-I DCI 4K_DCI 4K S35[10bit 4:2:2][50p]","XAVC S-I 4K_UHD S35[10bit 4:2:2][50p]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][50p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][50p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][50p][From 7K]" :
         return 500
-    case "XAVC HS 8K_8K FF[10bit 4:2:2][23.976p/25p/29.97p][From 8.6K]":
+    case "XAVC HS 8K_8K FF[10bit 4:2:2][23.976p/25p/29.97p][From 8.6K]","XAVC HS 8K_8K FF[10bit 4:2:2][23.976p/25p/29.97p][From 8.2K]":
         return 520
-    case "XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][59.94p]","XAVC S-I 4K_UHD FF[10bit 4:2:2][59.94p]","XAVC S-I DCI 4K_DCI 4K S35[10bit 4:2:2][59.94p]","XAVC S-I 4K_UHD S35[10bit 4:2:2][59.94p]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][59.94p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][59.94p][From 6K]" :
+    case "XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][59.94p]","XAVC S-I 4K_UHD FF[10bit 4:2:2][59.94p]","XAVC S-I DCI 4K_DCI 4K S35[10bit 4:2:2][59.94p]","XAVC S-I 4K_UHD S35[10bit 4:2:2][59.94p]","XAVC S-I DCI 4K_DCI 4K FF[10bit 4:2:2][59.94p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][59.94p][From 6K]","XAVC S-I 4K_UHD FF[10bit 4:2:2][59.94p][From 7K]" :
         return 600
     default :
         return 0
