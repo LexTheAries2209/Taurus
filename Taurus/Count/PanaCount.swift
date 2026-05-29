a62788a56afaeabb30b2d18613fbf56ab70c1728
LexTheAries2209 <673661658@qq.com>
2026-05-29 18:30:15 +0800

    修复了S1R2分辨率选择和计算错误的问题。

--- Taurus/Count/PanaCount.swift
+++ Taurus/Count/PanaCount.swift
@@ -58,7 +58,7 @@ func PanaS1R2CodecSpeed(_ PanaCameraCodecName:String) -> Double {
     case "S1R2_ProRes 422_DCI 4K[10bit 4:2:2][23.976p]" :
         return 519
     case "S1R2_ProRes 422_DCI 4K[10bit 4:2:2][24p]" :
-        return 520
+        return 519
     case "S1R2_ProRes 422_DCI 4K[10bit 4:2:2][25p]" :
         return 541
     case "S1R2_ProRes 422_DCI 4K[10bit 4:2:2][29.97p]" :
@@ -106,7 +106,7 @@ func PanaS1R2CodecSpeed(_ PanaCameraCodecName:String) -> Double {
     case "S1R2_ProRes 422_FHD[10bit 4:2:2][23.976p]" :
         return 121
     case "S1R2_ProRes 422_FHD[10bit 4:2:2][24p]" :
-        return 122
+        return 121
     case "S1R2_ProRes 422_FHD[10bit 4:2:2][25p]" :
         return 126
     case "S1R2_ProRes 422_FHD[10bit 4:2:2][29.97p]" :
@@ -119,6 +119,8 @@ func PanaS1R2CodecSpeed(_ PanaCameraCodecName:String) -> Double {
         return 505
     case "S1R2_ProRes 422_FHD[10bit 4:2:2][119.88p]" :
         return 601
+    case "S1R2_ProRes 422_FHD[10bit 4:2:2][120p]" :
+        return 606
     case "S1R2_ProRes 422 HQ_FHD[10bit 4:2:2][23.976p]" :
         return 181
     case "S1R2_ProRes 422 HQ_FHD[10bit 4:2:2][24p]" :
@@ -135,17 +137,19 @@ func PanaS1R2CodecSpeed(_ PanaCameraCodecName:String) -> Double {
         return 757
     case "S1R2_ProRes 422 HQ_FHD[10bit 4:2:2][119.88p]" :
         return 901
+    case "S1R2_ProRes 422 HQ_FHD[10bit 4:2:2][120p]" :
+        return 908
 
-    case "S1R2_HEVC LongGOP_8.1K 3:2[8064*5376][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_8.1K[8128*4288][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_8K[7680*4320][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_7.2K 3:2[7200*4800][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_6.4K 3:2[6432*4288][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_5.9K[5888*3312][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_5.8K[5760*3040][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_4.7K 4:3[4736*3552][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][100p/119.88p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][100p/119.88p]" :
+    case "S1R2_HEVC LongGOP_8.1K[8064*5376][10bit 4:2:0][23.976p/24p][OG]","S1R2_HEVC LongGOP_8.1K[8064*5376][10bit 4:2:0][23.976p/24p/25p/29.97p][OG]","S1R2_HEVC LongGOP_8.1K 3:2[8064*5376][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_7.2K[7200*4800][10bit 4:2:0][25p/29.97p][OG]","S1R2_HEVC LongGOP_7.2K[7200*4800][10bit 4:2:0][23.976p/24p/25p/29.97p][OG]","S1R2_HEVC LongGOP_7.2K 3:2[7200*4800][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_6.4K[6432*4288][10bit 4:2:0][23.976p/24p/25p/29.97p][OG]","S1R2_HEVC LongGOP_6.4K 3:2[6432*4288][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_8K[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_8.1K[8128*4288][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_8K[7680*4320][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_5.9K[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_5.9K[5888*3312][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_5.8K[5760*3040][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_4.7K 4:3[4736*3552][10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][100p/119.88p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][100p/119.88p]","S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][100p/119.88p/120p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][100p/119.88p/120p]" :
         return 300
-    case "S1R2_HEVC LongGOP_5.9K[5888*3312][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_5.8K[5760*3040][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_4.7K 4:3[4736*3552][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_AVC LongGOP_DCI 4K[10bit 4:2:2][47.95p/48p/50p/59.94p]","S1R2_AVC LongGOP_UHD[10bit 4:2:2][47.95p/48p/50p/59.94p]" :
+    case "S1R2_HEVC LongGOP_5.9K[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_5.9K[5888*3312][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_5.8K[5760*3040][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_4.7K 4:3[4736*3552][10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_AVC LongGOP_DCI 4K[10bit 4:2:2][47.95p/48p/50p/59.94p]","S1R2_AVC LongGOP_UHD[10bit 4:2:2][47.95p/48p/50p/59.94p]" :
         return 200
-    case "S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_FHD[10bit 4:2:0][100p/119.88p]","S1R2_AVC LongGOP_DCI 4K[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC LongGOP_UHD[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC LongGOP_UHD[10bit 4:2:2][100p/119.88p]","S1R2_AVC LongGOP_FHD[10bit 4:2:2][100p/119.88p]" :
+    case "S1R2_HEVC LongGOP_DCI 4K[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_UHD[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_FHD[10bit 4:2:0][100p/119.88p]","S1R2_HEVC LongGOP_FHD[10bit 4:2:0][100p/119.88p/120p]","S1R2_AVC LongGOP_DCI 4K[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC LongGOP_UHD[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC LongGOP_FHD[10bit 4:2:2][100p/119.88p]","S1R2_AVC LongGOP_FHD[10bit 4:2:2][100p/119.88p/120p]" :
         return 150
     case "S1R2_HEVC LongGOP_FHD[10bit 4:2:0][23.976p/24p/25p/29.97p]","S1R2_HEVC LongGOP_FHD[10bit 4:2:0][47.95p/48p/50p/59.94p]","S1R2_AVC LongGOP_FHD[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC LongGOP_FHD[10bit 4:2:2][47.95p/48p/50p/59.94p]" :
         return 100
 
-    case "S1R2_AVC ALL-I_DCI 4K[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC ALL-I_UHD[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC ALL-I_FHD[10bit 4:2:2][100p/119.88p]" :
+    case "S1R2_AVC ALL-I_DCI 4K[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC ALL-I_UHD[10bit 4:2:2][23.976p/24p/25p/29.97p]","S1R2_AVC ALL-I_FHD[10bit 4:2:2][100p/119.88p]","S1R2_AVC ALL-I_FHD[10bit 4:2:2][100p/119.88p/120p]" :
         return 400
     case "S1R2_AVC ALL-I_DCI 4K[10bit 4:2:2][47.95p/48p/50p/59.94p][Light]","S1R2_AVC ALL-I_UHD[10bit 4:2:2][47.95p/48p/50p/59.94p][Light]" :
         return 600
@@ -166,6 +170,9 @@ func PanaCodecSpeed(cameradata:CameraData) -> Double {
         let S1R2CodecSpeed = PanaS1R2CodecSpeed(PanaCameraCodecName)
         if S1R2CodecSpeed != 0 {
             return S1R2CodecSpeed
+        }
+        if cameradata.Codec == "AVC LongGOP" && (cameradata.Resolution == "UHD[10bit 4:2:2][100p/119.88p]" || cameradata.Resolution == "4K[10bit 4:2:2][100p/119.88p]" || cameradata.Resolution == "4K[10bit 4:2:2][100p/119.88p/120p]") {
+            return 0
         }
     }
     switch PanaCodecName {
